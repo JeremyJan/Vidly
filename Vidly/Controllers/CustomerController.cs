b638b6072672822f5ea980bf3f02a89122f97964
JeremyJan <Jeremy.manandic@gmail.com>
2020-11-28 10:43:44 -0800

    first commit

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        // GET: Customer
+        //[Route("Customers")]
+        public ViewResult Index()
+        {
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customer);
+        }
+
+        //[Route("Customers/")]
+        //TODO write details and fix code to make it the same as lecture.
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
